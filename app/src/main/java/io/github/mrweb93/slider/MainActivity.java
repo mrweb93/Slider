7d7bdfebc2b882013207ca25b72682bf781ec7be
mr_w_e_b <mr_w_e_b@me.com>
2021-06-27 23:41:01 +1100

    Initial commit

---
+++ app/src/main/java/io/github/mrweb93/slider/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.mrweb93.slider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
