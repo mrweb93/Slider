92ba8299b207b0d049982700834fa32e8d119d85
mr_w_e_b <mr_w_e_b@me.com>
2021-07-03 11:43:18 +1100

    Финиш

--- app/src/main/java/io/github/mrweb93/slider/MainActivity.java
+++ app/src/main/java/io/github/mrweb93/slider/MainActivity.java
@@ -1,14 +1,23 @@
 package io.github.mrweb93.slider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager=(ViewPager) findViewById(R.id.viewpager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
+
     }
 }
--- app/src/main/java/io/github/mrweb93/slider/SlideAdapter.java
+++ app/src/main/java/io/github/mrweb93/slider/SlideAdapter.java
@@ -5,64 +5,78 @@ import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
-
-import org.jetbrains.annotations.NotNull;
 
 public class SlideAdapter extends PagerAdapter {
     Context context;
     LayoutInflater inflater;
 
-    public int[] list_image={
-    R.drawable.image_1,
-    R.drawable.image_2,
-    R.drawable.image_3,
-    R.drawable.image_4
+    // list of images
+    public int[] lst_images = {
+            R.drawable.image_1,
+            R.drawable.image_2,
+            R.drawable.image_3,
+            R.drawable.image_4
     };
-
-    public String[] list_title={
+    // list of titles
+    public String[] lst_title = {
             "COSMONAUT",
             "SATELITE",
             "GALAXY",
             "ROCKET"
+    }   ;
+    // list of descriptions
+    public String[] lst_description = {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
     };
-
-    public String[] list_description={
-            "Description 1",
-            "Description 2",
-            "Description 3",
-            "Description 4"
-    };
-
-    public int[] list_background={
+    // list of background colors
+    public int[]  lst_backgroundcolor = {
             Color.rgb(55,55,55),
             Color.rgb(239,85,85),
             Color.rgb(110,49,89),
             Color.rgb(1,188,212)
     };
 
+
+    public SlideAdapter(Context context) {
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
-        return list_title.length;
+        return lst_title.length;
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return (view==(LinearLayout) object);
-    }
-
-    @NonNull
-    @NotNull
-    @Override
-    public Object instantiateItem(@NonNull @NotNull View container, int position) {
-        return super.instantiateItem(container, position);
+        return (view==(LinearLayout)object);
     }
 
     @Override
-    public void destroyItem(@NonNull @NotNull ViewGroup container, int position, @NonNull @NotNull Object object) {
-        super.destroyItem(container, position, object);
+    public Object instantiateItem(ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slide,container,false);
+        LinearLayout layoutslide = (LinearLayout) view.findViewById(R.id.slidelinearlayout);
+        ImageView imgslide = (ImageView)  view.findViewById(R.id.slideimg);
+        TextView txttitle= (TextView) view.findViewById(R.id.txttitle);
+        TextView description = (TextView) view.findViewById(R.id.txtdescription);
+        layoutslide.setBackgroundColor(lst_backgroundcolor[position]);
+        imgslide.setImageResource(lst_images[position]);
+        txttitle.setText(lst_title[position]);
+        description.setText(lst_description[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout)object);
     }
 }
